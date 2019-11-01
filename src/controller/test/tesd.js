4cdd5aba8af8424fc8404ec9e2fce33519ced919
o665p <515235416@qq.com>
2019-11-01 18:11:34 +0800

    修改bug

--- src/controller/test/tesd.js
+++ src/controller/test/tesd.js
@@ -4,7 +4,8 @@ var request = require('request');
 //获取经纬度
 module.exports = class extends Base {
     async indexAction() {
-          return this.fail(); //2输出json 推荐
+        let sdde = await this.model('lunbo').add({imgurl: 'xxx'});
+          return this.fail({id:sdde}); //2输出json 推荐
     }
 
 };
